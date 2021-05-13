82c6a95eb0958f383ee69db194e1b1f44afc94ae
Sergey Gan <googlgan@gmail.com>
2021-05-14 01:47:48 +0300

    Домашняя работа №1 2 запускаемых активити

--- app/src/main/java/net/gan/firstapp/MainActivity.java
+++ app/src/main/java/net/gan/firstapp/MainActivity.java
@@ -1,8 +1,17 @@
 package net.gan.firstapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.ToggleButton;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +19,46 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText editText = findViewById(R.id.edit_text1);
+        TextView textView = findViewById(R.id.text1);
+        ToggleButton toggleButton = findViewById(R.id.toggleButton);
+        SwitchCompat aSwitch = findViewById(R.id.switch1);
+        CheckBox checkBox = findViewById(R.id.checkBox);
+        Button button = findViewById(R.id.button);
+        ImageView image = findViewById(R.id.imageView);
+
+        toggleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (toggleButton.isChecked()){
+                    image.setVisibility(View.INVISIBLE);
+                }else{
+                    image.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                textView.setText(editText.getText().toString());
+
+                if (aSwitch.isChecked()){
+                    button.setBackgroundColor(button.getResources().getColor(R.color.additional_color_yellow));
+                }else{
+                    button.setBackgroundColor(button.getResources().getColor(R.color.button_color));
+                }
+
+                if (checkBox.isChecked()){
+                    textView.setVisibility(View.INVISIBLE);
+                }else{
+                    textView.setVisibility(View.VISIBLE);
+                }
+
+            }
+        });
+
     }
 }
